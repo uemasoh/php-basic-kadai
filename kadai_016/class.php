e9b3c21e1a9cfb331f31b0ccaba1905ec7b504c2
uemasoh <ue.masoh@gmail.com>
2025-11-17 18:03:20 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,35 @@
+<?php
+class Food {
+  private $name;
+  private $price;
+  public function show_price($price) {
+    echo $this -> price."<br>";
+  }
+  public function __construct($name,$price){
+    $this ->name = $name;
+    $this ->price = $price;
+  }
+  $food = new Food("potato",250);
+  print_r($food);
+  $food->show_price();
+}
+
+
+class Animal {
+  private $name;
+  private $height;
+  private $weight;
+  public function show_height($height) {
+    echo $this -> height."<br>";
+  }
+  public function __construct($name,$price){
+    $this ->name = $name;
+    $this ->price = $height;
+    $this ->weight = $weight;
+  }
+  $animal = new Animal("dog",60,5000);
+  print_r($animal);
+  $animal->show_height();
+}
+
+?>
